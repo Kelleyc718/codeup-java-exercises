97b5cfaff3c07143b01792833b9bc55519647003
Chris Kelley <kelleyc718@gmail.com>
2018-02-27 16:13:40 -0600

    Loop exercise 1

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -2,10 +2,24 @@ import java.util.Scanner;
 
 public class ConsoleExercises {
     public static void main(String[] args) {
-        String userInput;
+        String length;
+        String width;
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number please...");
-        userInput = sc.nextLine();
-        System.out.println("You entered: --> \"" + userInput + "\" <-- ");
+
+        System.out.println("Enter the length of the class room");
+        length = sc.nextLine();
+
+        System.out.println("Enter the width of the class room");
+        width = sc.nextLine();
+
+        int perimeter = (int) (2 * Double.parseDouble(length)) + (int) (2 * Double
+                .parseDouble(width));
+        int area = Float.valueOf(length).intValue() * Float.valueOf(width).intValue();
+        int volume = area * 9;
+
+        System.out.println("Area: " + area);
+        System.out.println("Perimeter: " + perimeter);
+        System.out.println("Volume: " + volume);
     }
 }
