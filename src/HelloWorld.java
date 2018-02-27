795474c71cd74e8ada80c3cd1abf88da377f4812
Chris Kelley <kelleyc718@gmail.com>
2018-02-27 11:18:31 -0600

    Complete exercises with variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        int myFavoriteNumber = 8;
+        int x = 3;
+        int y = 4;
+            x /= 5;
+            y -= x;
+        System.out.println(x);
+        System.out.println(y);
         System.out.println("Hello, World!");
-        System.out.print("Hello, World!");
     }
 }
