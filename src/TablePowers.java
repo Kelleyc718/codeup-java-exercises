24895f2763632f982247f7e07c2799b897b096f0
Chris Kelley <kelleyc718@gmail.com>
2018-02-27 22:17:11 -0600

    use printf to format output

---
+++ src/TablePowers.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class TablePowers {
+    public static void main(String[] args) {
+        String userInput;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What number would you like to go up to?");
+        userInput = sc.nextLine();
+        int usrNum = Integer.parseInt(userInput);
+
+        System.out.println("Here is your table!");
+        System.out.println("number |  squared   | cubed");
+        System.out.println("------ |  -------   | -----");
+
+        for(int i = 1; i <= usrNum; i++) {
+            int squared = (int) Math.pow(i, 2);
+            int cubed = (int) Math.pow(i, 3);
+            System.out.print(i);
+            System.out.printf("%8s", "| ");
+
+            System.out.print(squared);
+            if (squared >= 10) {
+                System.out.printf("%11s", "| ");
+            } else {
+                System.out.printf("%12s", "| ");
+            }
+            System.out.print(cubed);
+            System.out.print("\n");
+        }
+    }
+}
