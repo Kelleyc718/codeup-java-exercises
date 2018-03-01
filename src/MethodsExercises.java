1b685eee9ae85145cc015b8c28770e03573f4bc0
Chris Kelley <kelleyc718@gmail.com>
2018-03-01 16:19:32 -0600

    refactor some of the method exercises

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class MethodsExercises {
     public static void main(String[] args) {
         int a = getInteger(1, 10);
-        int b = getInteger(1, 10);
+        int b = getInteger(a, 10);
 
         System.out.println("Add: " + add(a, b));
         System.out.println("Subtract: " + subtract(a, b));
@@ -12,7 +12,7 @@ public class MethodsExercises {
         System.out.println("Divide: " + divide(a, b));
         System.out.println("Modulus: " + modulus(a, b));
         System.out.println(" = " + factorial(a));
-        System.out.println(diceRoll(a, b));
+        diceRoll(a, b);
 
     }
 
@@ -24,11 +24,11 @@ public class MethodsExercises {
         return a - b;
     }
 
-    public static int multiply(int a, int b) {
+    public static double multiply(double a, double b) {
         return a * b;
     }
 
-    public static int divide(int a, int b) {
+    public static double divide(double a, double b) {
         return a / b;
     }
 
@@ -38,64 +38,43 @@ public class MethodsExercises {
 
     public static int getInteger(int a, int b) {
         Scanner sc = new Scanner(System.in);
-        int userInput;
-
         System.out.println("Enter a number between " + a + " and " + b + ".");
-        userInput = sc.nextInt();
-
+        int userInput = sc.nextInt();
         if (userInput < a || userInput > b) {
             System.out.println("Please enter a valid number within the range of " + a + " and " + b + ".");
-            userInput = sc.nextInt();
+            getInteger(a, b);
         }
         return userInput;
     }
 
-    public static long factorial(int a) {
-        int total = 0;
-        System.out.print(a + "! = ");
-        for (int i = 1; i <= a; i++) {
-            if (i == a) {
+    public static long factorial(int num) {
+        long total = 1;
+        System.out.print(num + "! = ");
+        for (int i = 1; i <= num; i++) {
+            if (i == num) {
                 System.out.print(i);
             } else {
                 System.out.print(i + " x ");
-                total += (i * (i + 1));
+                total += total * (i + 1);
             }
         }
         return total;
     }
 
-    public static String diceRoll(int a, int b) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("You have been given two dice...");
-
-        String input;
-        System.out.println("Your dice have " + a + " and " + b + " sides.");
-        System.out.println("Type \"Roll\" to roll your dice...");
-        input = sc.next();
-
-        while (!input.equals("Roll")) {
-            System.out.println("TRY AGAIN!");
-            System.out.println("Type \" Roll \" to roll your dice...");
-            input = sc.next();
-        }
+    public static void diceRoll(int a, int b) {
         int resultOne = randomNum(a);
         int resultTwo = randomNum(b);
         System.out.println("You rolled a " + resultOne + " and " + resultTwo + '.');
+            if (cont()) {
+                diceRoll(a, b);
+            }
+    }
 
+    public static boolean cont() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Would you like to roll again?");
-        input = sc.next();
-
-        while (input.equalsIgnoreCase("y")) {
-            resultOne = randomNum(a);
-            resultTwo = randomNum(b);
-            System.out.println("You rolled a " + resultOne + " and " + resultTwo + '.');
-
-            System.out.println("Would you like to roll again?");
-            input = sc.next();
-        }
-
-        return "Have a nice day";
+        String input = sc.next();
+        return (input.equalsIgnoreCase("y"));
     }
 
     public static int randomNum(int num) {
