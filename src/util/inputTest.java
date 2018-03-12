6dc83f6dd08473bcec1f6a0061996d8fe993cc14
Chris Kelley <kelleyc718@gmail.com>
2018-03-12 14:07:12 -0500

    Finished filehelper tool

--- src/util/inputTest.java
+++ src/util/inputTest.java
@@ -1,13 +1,9 @@
 package util;
 
+
 public class inputTest {
     public static void main(String[] args) {
-        Input myTest = new Input();
-        System.out.println(myTest.getString());
-        System.out.println(myTest.yesNo());
-        System.out.println(myTest.getInt());
-        System.out.println(myTest.getInt(1, 100));
-        System.out.println(myTest.getDouble());
-        System.out.println(myTest.getDouble(1.0, 99.99));
+        //System.out.printf("%s", FileHelper.slurp("src/Person.java"));
+        FileHelper.spit("text4.txt", FileHelper.slurp("src/util/input.java"), true);
     }
 }
